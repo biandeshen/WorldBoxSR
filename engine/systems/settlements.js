22abadff9cf2df243da7c10046ebdc3f8c6633a9
fanjiangpan <biandeshen@gmail.com>
2026-08-24 11:47:00 +0800

    revert(storage): remove experimental settlement lifecycle hooks

--- engine/systems/settlements.js
+++ engine/systems/settlements.js
@@ -1,11 +1,6 @@
 import { createSettlement } from '../model/settlement.js';
 import { entityRef, pushEvent } from '../model/events.js';
 import { tileAt } from '../core/world.js';
-import {
-  initializeSettlementFoodStorage,
-  recordSettlementFoodStorageAbandonment,
-  updateSettlementFoodStorage
-} from './settlement_food_storage.js';
 
 export function updateSettlements(world) {
   const interval = world.config.settlementCheckIntervalDays;
@@ -45,7 +40,6 @@ export function updateSettlements(world) {
   for (const { tile } of eligible) {
     if (tooCloseToSettlement(world, tile.x, tile.y)) continue;
     const settlement = createSettlement(world, { x: tile.x, y: tile.y });
-    initializeSettlementFoodStorage(world, settlement);
     tile.settlementCandidateDays = 0;
     pushEvent(world, {
       type: 'settlement.founded',
@@ -60,7 +54,6 @@ export function updateSettlements(world) {
   updateSettlementMembership(world);
   updateSettlementLifecycle(world, interval);
   updateSettlementTerritory(world);
-  updateSettlementFoodStorage(world);
 }
 
 export function updateSettlementMembership(world) {
@@ -95,7 +88,6 @@ export function updateSettlementLifecycle(world, interval = world.config.settlem
     settlement.abandonedDay = world.day;
     settlement.population = 0;
     settlement.memberIds = [];
-    recordSettlementFoodStorageAbandonment(world, settlement);
     pushEvent(world, {
       type: 'settlement.abandoned',
       subject: entityRef('settlement', settlement.id),
